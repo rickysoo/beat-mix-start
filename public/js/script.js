515f501f1aba2a5b7737c45b0aba6ca0861459ec
Ricky Soo <8564646+rickysoo@users.noreply.github.com>
2020-04-21 17:46:36 +0800

    Set up array functions

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,70 @@
 // Drum Arrays
-let kicks
-let snares
-let hiHats
-let rideCymbals
+const kicks = new Array(16).fill(false);
+const snares = new Array(16).fill(false);
+const hiHats = new Array(16).fill(false);
+const rideCymbals = new Array(16).fill(false);
 
+function getArrayByName(arrayName) {
+    switch (arrayName) {
+        case 'kicks':
+            return kicks;
+        case 'snares':
+            return snares;
+        case 'hiHats':
+            return hiHats;
+        case 'rideCymbals':
+            return rideCymbals;
+        default:
+            return undefined;
+    }
+}
+
+function toggleDrum(arrayName, index) {
+
+
+    if (arrayName === undefined || index === undefined) {
+        return;
+    }
+
+    array = getArrayByName(arrayName);
+
+    if (arrayName === undefined || index < 0 || index > array.length) {
+        return;
+    }
+
+    array[index] = !array[index];
+}
+
+function clear(arrayName) {
+    if (arrayName === undefined) {
+        return;
+    }
+
+    array = getArrayByName(arrayName);
+
+    if (array === undefined) {
+        return;
+    }
+
+    array.fill(false);
+}
+
+function invert(arrayName) {
+    if (arrayName === undefined) {
+        return;
+    }
+
+    array = getArrayByName(arrayName);
+
+    if (array === undefined) {
+        return;
+    }
+
+    for (i = 0; i < array.length; i++) {
+        array[i] = !array[i];
+    }
+}
+
+// console.log(`Before: ${kicks}`);
+// toggleDrum('kicks', 0);
+// console.log(`After: ${kicks}`);
